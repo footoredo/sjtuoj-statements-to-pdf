d631b6ebd15303991f7ed2087e4c3bc1722e4fca
footoredo <footoredo@gmail.com>
2018-07-04 16:42:19 +0800

    now support merge

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ A script to crawl & convert HTML statements on SJTUOJ.
 
 ## Requirements
 
-[pdfkit](https://github.com/JazzCore/python-pdfkit/)
+- [pdfkit](https://github.com/JazzCore/python-pdfkit/)
+- [PyPDF2](https://github.com/mstamy2/PyPDF2)
 
 ## Usage
 
 ```
-usage: sjtuoj2pdf.py [-h] [-n NJOBS] [-z ZOOM] start_id [stop_id]
+usage: sjtuoj2pdf.py [-h] [-n NJOBS] [-z ZOOM] [-o OUTPUT] start_id [stop_id]
 
 Crawl & convert HTML problem statements on sjtuoj to pdf.
 
@@ -22,9 +23,11 @@ optional arguments:
   -n NJOBS, --njobs NJOBS
                         multi-thread limit (default = 5)
   -z ZOOM, --zoom ZOOM  zoom parameter (default = 1.0)
+  -o OUTPUT, --output OUTPUT
+                        output file name
 ```
 
-Remeber to create `output/` and fill the `username` and `password`. PDF version of problems will be saved to `output/[prob_id].pdf`.
+Remeber to create `output/` and fill the `username` and `password`. PDF version of problems will be saved to `output/[prob_id].pdf`. If you specify a `--output` file, all statements will be merged into that file.
 
 ## Examples
 
@@ -34,10 +37,10 @@ Remeber to create `output/` and fill the `username` and `password`. PDF version 
 python sjtuoj2pdf.py 8652
 ```
 
-- Crawl a series of problems [8652, 8662] on 3 threads
+- Crawl a series of problems [8652, 8662] on 3 threads and merge them into `statements.pdf`
 
 ```
-python sjtuoj2pdf.py --njobs 3 8652 8662
+python sjtuoj2pdf.py --njobs 3 --output statements.pdf 8652 8662
 ```
 
 - Zoom the statements to 90%
--- sjtuoj2pdf.py
+++ sjtuoj2pdf.py
@@ -2,6 +2,25 @@ import pdfkit
 import argparse
 import requests
 from multiprocessing import Pool
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+def merge (output_path, input_paths):
+    print ("Merging PDFs ...")
+
+    pdf_writer = PdfFileWriter ()
+
+    for pdf_index, path in enumerate (input_paths):
+        pdf_reader = PdfFileReader (path)
+        print ("Processing PDF {} (out of {}), pages: {}".format (
+            pdf_index, len(input_paths), pdf_reader.getNumPages ()))
+        for page in range (pdf_reader.getNumPages ()):
+            pdf_writer.addPage (pdf_reader.getPage (page))
+
+    with open (output_path, 'wb') as fh:
+        pdf_writer.write (fh)
+        
+    print ("Done.")
+
 
 sjtuoj = "https://sjtuoj.weak.cat"
 #sjtuoj = "http://176.16.45.6"
@@ -26,9 +45,10 @@ def requests2wkhtmltopdf (cookies):
 
 statement = "/problems/{}/html/"
 options = {
-        'encoding': "UTF-8"
+        'encoding': "utf-8"
         }
-output = "output/{}.pdf"
+output_dir = "output/"
+output = output_dir + "/{}.pdf"
 
 def crawl (prob_id):
     url = sjtuoj + statement.format (prob_id)
@@ -40,6 +60,7 @@ if __name__ == "__main__":
     parser.add_argument ("stop_id", nargs = '?', type = int, help = "stop ID (inclusive) of target problem(s)")
     parser.add_argument ("-n", "--njobs", type = int, help = "multi-thread limit (default = 5)", default = 5)
     parser.add_argument ("-z", "--zoom", type = float, help = "zoom parameter (default = 1.0)", default = 1)
+    parser.add_argument ("-o", "--output", type = str, help = "output file name")
     args = parser.parse_args ()
 
     print ("Logining ...")
@@ -56,3 +77,6 @@ if __name__ == "__main__":
 
     with Pool (args.njobs) as p:
         p.map (crawl, r)
+
+    if args.output != None:
+        merge (output_dir + args.output, list (map (output.format, r)))
